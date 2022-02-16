2c3e3983b17ae8ea96d78bfa7c52bfefb5076407
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-02-16 22:09:28 +0200

    Improve jsdocs

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,45 @@ import type { AnyRouter, Dict } from '@trpc/server';
 import { resolveHTTPResponse } from '@trpc/server';
 import { CreateContextFn, ResponseMetaFn } from './types';
 
+/**
+ * A function that creates a tRPC handle.
+ * @see https://kit.svelte.dev/docs/hooks
+ */
 export function createTRPCHandle<Router extends AnyRouter>({
   url = '/trpc',
   router,
   createContext,
   responseMeta,
 }: {
-  /** @default '/trpc' */
+  /**
+   * The URL prefix of tRPC routes.
+   * Must start with `/` and NOT end with `/`.
+   * Requests starting with this prefix will be intercepted and handled by tRPC,
+   * and will NOT be forwarded to SvelteKit.
+   * @default '/trpc' */
   url?: string;
+
+  /**
+   * The tRPC router
+   * @see https://trpc.io/docs/router */
   router: Router;
+
+  /**
+   * A function called for each request, whose result is propagated to all resolvers.
+   * You can use this to pass contextual data down to the resolvers.
+   * @see https://trpc.io/docs/context */
   createContext?: CreateContextFn<Router>;
+
+  /**
+   * A function allowing you to override/customize the response status and headers
+   * (i.e. to control caching).
+   * @see https://trpc.io/docs/caching */
   responseMeta?: ResponseMetaFn<Router>;
 }): Handle {
+  if (!url.startsWith('/') || url.endsWith('/')) {
+    throw new Error("The tRPC url must start with '/' and NOT end with '/'");
+  }
+
   return async function ({ event, resolve }) {
     if (event.url.pathname.startsWith(`${url}/`)) {
       const request = event.request as Request & { headers: Dict<string | string[]> };
