6b4f1914cc2a42eecd99883097cf6964703410dc
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-11-28 23:52:25 +0200

    Change docs appDir

--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -8,7 +8,8 @@ const config = {
   preprocess: preprocess(),
   kit: {
     adapter: adapter(),
-    paths: { base: process.env.GITHUB_PAGES === 'true' ? '/trpc-sveltekit' : '' }
+    paths: { base: process.env.GITHUB_PAGES === 'true' ? '/trpc-sveltekit' : '' },
+    appDir: 'internal'
   }
 };
 
