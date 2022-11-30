d8cd49747cb8f30141d986133300345d61800fe5
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-11-30 18:37:33 +0200

    Docs website: fix sitemap

--- docs/src/routes/sitemap.xml/+server.ts
+++ docs/src/routes/sitemap.xml/+server.ts
@@ -11,7 +11,9 @@ export const GET: RequestHandler = () => {
       '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">',
       ...PAGES.map(
         ({ path }) =>
-          `<url><loc>${GITHUB_PAGES_ROOT}${path}</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.7</priority></url>`
+          `<url><loc>${GITHUB_PAGES_ROOT}${
+            path === '/' ? '' : path
+          }</loc><lastmod>${date}</lastmod><changefreq>weekly</changefreq><priority>0.7</priority></url>`
       ),
       '</urlset>'
     ].join('\n')
