741cfcdcebfefefc714dfbd36bb0d507315779ca
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-02-03 15:45:54 +0200

    Remove types from layout server load in example

--- examples/bookstall/src/routes/+layout.server.ts
+++ examples/bookstall/src/routes/+layout.server.ts
@@ -2,13 +2,11 @@ import { JWT_SECRET } from '$env/static/private';
 import jwt from 'jsonwebtoken';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad<
-  { isAuthenticated?: false; userName?: never } | { isAuthenticated: true; userName: string }
-> = async ({ cookies }) => {
+export const load: LayoutServerLoad = async ({ cookies }) => {
   try {
     const { name: userName } = jwt.verify(cookies.get('jwt') || '', JWT_SECRET) as { name: string };
     return { isAuthenticated: true, userName };
   } catch {
-    return { isAuthenticated: false };
+    return { isAuthenticated: false, userName: '' };
   }
 };
