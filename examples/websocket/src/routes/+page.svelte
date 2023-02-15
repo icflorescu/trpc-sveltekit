ba67456d1d0ecefd1ff674c4a31e0eef9d07b13d
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-02-15 14:39:46 +0200

    Use pico.css in example

--- examples/websocket/src/routes/+page.svelte
+++ examples/websocket/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { trpc } from '$lib/trpc/client';
+  import '@picocss/pico';
 
   let greeting = 'press the button to load data';
   let loading = false;
@@ -11,13 +12,25 @@
   };
 </script>
 
-<h6>Loading data in<br /><code>+page.svelte</code></h6>
+<main class="container">
+  <h6>Loading data in<br /><code>+page.svelte</code></h6>
 
-<a
-  href="#load"
-  role="button"
-  class="secondary"
-  aria-busy={loading}
-  on:click|preventDefault={loadData}>Load</a
->
-<p>{greeting}</p>
+  <a
+    href="#load"
+    role="button"
+    class="secondary"
+    aria-busy={loading}
+    on:click|preventDefault={loadData}>Load</a
+  >
+  <p>{greeting}</p>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+</style>
