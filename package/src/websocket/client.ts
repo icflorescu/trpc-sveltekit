b5f68baec6dc8615ac62d7172fd9d447b43b65a1
vishalbalaji <vishalbalaji@xyz>
2023-02-22 18:44:08 +0530

    fix: prevent from creating client if `location` is `undefined`

--- package/src/websocket/client.ts
+++ package/src/websocket/client.ts
@@ -7,6 +7,8 @@ import {
 import { AnyRouter } from '@trpc/server';
 
 export function createTRPCWebSocketClient<Router extends AnyRouter>() {
+  if (typeof location === 'undefined') return;
+
   const uri = `${location.protocol === 'http:' ? 'ws:' : 'wss:'}//${location.host}/trpc`;
 
   const wsClient = createWSClient({
