faeed3159e723a4927bd2103c649c38fc2998168
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-03-25 14:01:42 +0200

    Allow providing custom tRPC client links

--- package/src/client.ts
+++ package/src/client.ts
@@ -8,62 +8,74 @@ import type { AnyRouter } from '@trpc/server';
 
 export type TRPCClientInit = { fetch?: typeof window.fetch; url: { origin: string } };
 
+type CreateTRPCClientOptions<Router extends AnyRouter> = (
+  | {
+      links?: never;
+
+      /**
+       * The tRPC api endpoint URL.
+       * @default '/trpc'
+       */
+      url?: `/${string}`;
+
+      /**
+       * A page store or SvelteKit load event.
+       * @see https://kit.svelte.dev/docs/modules#$app-stores
+       * @see https://kit.svelte.dev/docs/load
+       */
+      init?: TRPCClientInit;
+
+      /**
+       * Additional headers to send with the request. Can be a function that returns headers.
+       * @see https://developer.mozilla.org/en-US/docs/Web/API/Headers
+       */
+      headers?: HTTPHeaders | (() => HTTPHeaders | Promise<HTTPHeaders>);
+    }
+  | {
+      /**
+       * A custom list of links to use for the tRPC Client instead of the default one.
+       * @see https://trpc.io/docs/links
+       */
+      links: TRPCLink<Router>[];
+
+      url?: never;
+      init?: never;
+      headers?: never;
+    }
+) & {
+  /**
+   * A function that transforms the data before transferring it.
+   * @see https://trpc.io/docs/data-transformers
+   */
+  transformer?: Router['_def']['_config']['transformer'];
+};
+
 /**
  * Create a tRPC client.
  * @see https://trpc.io/docs/vanilla
  */
 export function createTRPCClient<Router extends AnyRouter>(
-  {
-    url = '/trpc',
-    transformer,
-    init,
-    headers
-  }: {
-    /**
-     * The tRPC api endpoint URL.
-     * @default '/trpc'
-     */
-    url?: `/${string}`;
-
-    /**
-     * A function that transforms the data before transferring it.
-     * @see https://trpc.io/docs/data-transformers
-     */
-    transformer?: Router['_def']['_config']['transformer'];
-
-    /**
-     * A page store or SvelteKit load event.
-     * @see https://kit.svelte.dev/docs/modules#$app-stores
-     * @see https://kit.svelte.dev/docs/load
-     */
-    init?: TRPCClientInit;
-
-    /**
-     * Additional headers to send with the request. Can be a function that returns headers.
-     * @see https://developer.mozilla.org/en-US/docs/Web/API/Headers
-     */
-    headers?: HTTPHeaders | (() => HTTPHeaders | Promise<HTTPHeaders>);
-  } = { url: '/trpc' }
+  { links, url = '/trpc', transformer, init, headers }: CreateTRPCClientOptions<Router> = {
+    url: '/trpc'
+  }
 ) {
-  let link: TRPCLink<Router>;
+  if (links) return createTRPCProxyClient<Router>({ transformer, links });
 
-  if (typeof window === 'undefined') {
-    if (!init) {
-      throw new Error(
-        'Calling createTRPCClient() on the server requires passing a valid LoadEvent argument'
-      );
-    }
-
-    const {
-      fetch,
-      url: { origin }
-    } = init;
-
-    link = httpBatchLink({ url: `${origin}${url}`, fetch, headers });
-  } else {
-    const fetch = init?.fetch ?? window.fetch;
-    link = httpBatchLink({ url: `${location.origin}${url}`, fetch, headers });
+  if (typeof window === 'undefined' && !init) {
+    throw new Error(
+      'Calling createTRPCClient() on the server requires passing a valid LoadEvent argument'
+    );
   }
 
-  return createTRPCProxyClient<Router>({ transformer, links: [link] });
+  return createTRPCProxyClient<Router>({
+    transformer,
+    links: [
+      httpBatchLink({
+        url:
+          typeof window === 'undefined' ? `${init.url.origin}${url}` : `${location.origin}${url}`,
+        fetch: typeof window === 'undefined' ? init.fetch : init?.fetch ?? window.fetch,
+        headers
+      })
+    ]
+  });
 }
