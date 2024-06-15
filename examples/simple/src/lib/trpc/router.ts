5a1c801dac73d3d56b47dee847090248b70ec5ea
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2024-06-15 13:33:29 +0300

    Update deps

--- examples/simple/src/lib/trpc/router.ts
+++ examples/simple/src/lib/trpc/router.ts
@@ -7,7 +7,7 @@ export const t = initTRPC.context<Context>().create();
 export const router = t.router({
   greeting: t.procedure.query(async () => {
     await delay(500); // 👈 simulate an expensive operation
-    return `Hello tRPC v11 @ ${new Date().toLocaleTimeString()}`;
+    return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()}`;
   })
 });
 
