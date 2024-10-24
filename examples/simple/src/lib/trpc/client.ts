54bb925f0fd14d5184a31f0fb5211fd99e9424db
Pawel Paradysz <pa.paradysz@gmail.com>
2024-10-24 08:45:01 +0100

    Update client.ts
    For some reason, type inference on this file fails during build step.
    Which means that if this code is put in a monorepo and becomes part of a package, the .d.ts file doesn't get generated, and when trpc is imported, the whole type inference gets lost.
    
    This little (mostly) documentation fix will ensure that people who come to tRPC-sveltekit, and copy code from the docs, will never encounter this issue.

--- examples/simple/src/lib/trpc/client.ts
+++ examples/simple/src/lib/trpc/client.ts
@@ -1,9 +1,10 @@
 import type { Router } from '$lib/trpc/router';
 import { createTRPCClient, type TRPCClientInit } from 'trpc-sveltekit';
 
-let browserClient: ReturnType<typeof createTRPCClient<Router>>;
+type TRPCClient = ReturnType<typeof createTRPCClient<Router>>;
+let browserClient: TRPCClient;
 
-export function trpc(init?: TRPCClientInit) {
+export function trpc(init?: TRPCClientInit): TRPCClient {
   const isBrowser = typeof window !== 'undefined';
   if (isBrowser && browserClient) return browserClient;
   const client = createTRPCClient<Router>({ init });
